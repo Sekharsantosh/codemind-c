1cff7b1474554238478f0884d9d074d9e270bd2c
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-03-03 10:21:37 +0530

    #include <stdio.h> int main() {     char str[64];     int i = 0;     int sum = 0;     scanf("%s", str);
        while (str[i] != 0)
        {
            if ((str[i] >= '0') && (str[i] <= '9'))
                sum += (str[i]-'0');
            i++;
        }
        printf("%d", sum);
    
        return 0;
    }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char str[64];
+    int i = 0;
+    int sum = 0;
+    scanf("%s", str);
+
+    while (str[i] != 0)
+    {
+        if ((str[i] >= '0') && (str[i] <= '9'))
+            sum += (str[i]-'0');
+        i++;
+    }
+    printf("%d", sum);
+
+    return 0;
+}
