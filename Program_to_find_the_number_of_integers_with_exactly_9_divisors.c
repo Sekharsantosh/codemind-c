40e4f67b043ce69eef148efb363f939803e1ade5
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-29 18:54:35 +0530

    #include<stdio.h> int count(int n) {     int i,c=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c=c+1;         }     }     if(c==9)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(count(i))         {             printf("%d ",i);             c=c+1;         }     }     printf(" Total=%d",c); }

---
+++ Program_to_find_the_number_of_integers_with_exactly_9_divisors.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int count(int n)
+{
+    int i,c=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c=c+1;
+        }
+    }
+    if(c==9)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(count(i))
+        {
+            printf("%d ",i);
+            c=c+1;
+        }
+    }
+    printf("
+Total=%d",c);
+}
